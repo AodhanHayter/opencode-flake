06750f74fdac20e743604af60bb6f22828088deb
ßingen <bingen@eguzkitza.com>
2026-01-13 18:07:16 +0000

    Update OpenCode to version 1.1.18

--- package.nix
+++ package.nix
@@ -22,7 +22,7 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "opencode";
-  version = "1.0.203";
+  version = "1.1.18";
   src = fetchFromGitHub {
     owner = "sst";
     repo = "opencode";
